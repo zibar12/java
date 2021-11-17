990ad04226b2f13c6a68c6fb62899fdc0750d61d
yakup <93141026+zibar12@users.noreply.github.com>
2021-11-17 14:52:08 +0300

    ibanı_altalta_yazma.java
    girilen ibanı alt alta yazma...

---
+++ ibanı_altalta_yazma.java
@@ -0,0 +1,38 @@
+package ilk;
+
+import java.util.Scanner;
+
+public class proje24 {
+
+	public static void main(String[] args) {
+		// girilen iban numarasını 4 er 4er alt alta yazan programı kodlayın
+		Scanner sca=new Scanner(System.in);
+		
+		System.out.println("iban giriniz..:");
+		String iba=sca.nextLine();
+		String ilk;
+		int say=4;
+		int s3=4;
+		int s2=0;
+		int uzu=iba.length();
+		if(uzu==16) {
+			
+			for(int i=say=0;say<4;say++) {
+				
+				String s1 = iba.substring(s2,s3);
+				System.out.println(s1);
+				s3=s3+4;
+				s2=s2+4;
+			}
+			
+			
+		}
+		else {
+			System.out.println("hatalı girdiniz...");
+			
+			
+		}
+		
+	}
+
+}
