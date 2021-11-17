91b9413255c7ecfaf9dfcc6345525859c3afff9a
yakup <93141026+zibar12@users.noreply.github.com>
2021-11-17 15:20:49 +0300

    sayı_girme_prj4.java
    sayı girme prj4

---
+++ sayı_girme_prj4.java
@@ -0,0 +1,16 @@
+package ilk;
+
+import java.util.Scanner;
+
+public class proje4 {
+
+	public static void main(String[] args) {
+		
+		Scanner x = new Scanner(System.in);
+		System.out.println("sayı giriniz");
+		Integer a =x.nextInt();
+		System.out.println(a);
+		
+	}
+
+}
