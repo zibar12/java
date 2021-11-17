40e0b0e248a456058f7bb18cbd32025828e17321
yakup <93141026+zibar12@users.noreply.github.com>
2021-11-17 14:49:54 +0300

    tek_sayıları_toplama.java
    ( - ) li sayı girilene kadar tek sayıları toplama...

---
+++ tek_sayıları_toplama.java
@@ -0,0 +1,33 @@
+package ilk;
+
+import java.util.Scanner;
+
+public class proje27 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sca=new Scanner(System.in);
+		int s2=0;
+		while(true) {
+			
+			int s1;
+			System.out.println("sayı giriniz..:");
+			s1=sca.nextInt();
+		if(s1%2==1) {
+				
+				s2=s1+s2;
+				
+				
+		}	
+		if(s1<0) {
+			
+			System.out.println(s2);
+			
+			
+			}
+		
+		}
+
+	}
+
+}
