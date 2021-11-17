3f7e5bfd77cf548681b8bbd8dcd1459b2a3feabf
yakup <93141026+zibar12@users.noreply.github.com>
2021-11-17 14:38:11 +0300

    verilen_sayıya_prj13.java
    kullanıcını girdiği sayıya kadar toplama..

---
+++ verilen_sayıya_prj13.java
@@ -0,0 +1,27 @@
+package ilk;
+
+import java.util.Scanner;
+
+public class proje13 {
+
+	public static void main(String[] args) {
+		Scanner sca=new Scanner(System.in);
+		System.out.println("kaç defa bastırılsın:");
+		int i=sca.nextInt();
+		int x=0;
+		for(int f=1; f<=i; f++) {
+			
+			x=x+f;
+			
+			
+			
+			
+			
+			
+		}
+		System.out.println(x);
+		
+
+	}
+
+}
