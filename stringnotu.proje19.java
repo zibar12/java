4eb39b2973ea4d1815cda0afba758832f29838e8
yakup <93141026+zibar12@users.noreply.github.com>
2021-11-17 15:05:26 +0300

    stringnotu.proje19.java
    notlar

---
+++ stringnotu.proje19.java
@@ -0,0 +1,52 @@
+package ilk;
+
+public class proje19 {
+
+	public static void main(String[] args) {
+		/*javad string fonksiyonlar...:
+		 * length ayrırır....
+		 * 
+		 */
+		/*String st1="türkiye ";
+		int i;
+		int uzunluk=st1.length();
+		for(i=0;i<=st1.length()-1;i++) {
+			System.out.println("");
+			System.out.println(st1.charAt(i));
+			
+			*/
+		//büyük küçük harfler yazma
+		
+		//----------------------------------------------------------------------------------
+		/*String st1="TURKİYE";
+		System.out.println("küçük harfler "+st1.toLowerCase());//toLowerCase == küçük harfler
+		System.out.println("büyük harfler "+st1.toUpperCase());//toUpperCase == büyük harfler
+		*/
+		//-------------------------------------------------------------------------------------
+		//trim komutu bosluk yapmaz
+		/*String mystring= "Merhaba dunya";
+		mystring="Merhaba dunya";
+		System.out.println("bosluk yoktur " + mystring.trim());
+		*/
+		//-------------------------------------------------------------------------------------
+		//metin parçalama komutu split
+		/*String mystr="Merhaba dunya";
+		String[] words= mystr.split(" ");
+		*/
+		//---------------------------------------------------------------
+		//String yazi="the ve Kod ile Program";
+		//asagıda 0 ıncı karekteden 3 üncüye kadar
+		/*String ilk=yazi.substring(0,3);
+		System.out.println(ilk);*/
+		//6 -9 kadar
+		/*String iki=yazi.substring(6,9);
+		System.out.println(iki);
+		//15 den 20 kadar...
+		String son=yazi.substring(15,20);
+		System.out.println(son);
+		*/
+		}
+		
+		
+		
+	}
